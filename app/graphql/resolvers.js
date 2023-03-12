d1410e3d4a8cd63673b09342d825f0e55f84a3b0
Manav Misra <manavm@visionwebsoft.com>
2023-03-12 18:41:15 -0500

    fix: make resolver match typedef name

--- app/graphql/resolvers.js
+++ app/graphql/resolvers.js
@@ -3,7 +3,7 @@ import classController from "../class/controller.js";
 const resolvers = {
   Query: {
     // We must not return a promise in a resolver
-    async indexClasses() {
+    async classes() {
       // Resolve the promise and return the result (not a promise)
       return await classController.index();
     },
