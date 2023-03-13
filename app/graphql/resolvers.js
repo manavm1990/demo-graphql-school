891171d63c9aa143bba1efe8e9be244a65c0c7ea
Manav Misra <manavm@visionwebsoft.com>
2023-03-12 23:21:53 -0500

    docs: :recycle: reduce comment 💡 clutter

--- app/graphql/resolvers.js
+++ app/graphql/resolvers.js
@@ -2,7 +2,6 @@ import classController from "../class/controller.js";
 
 const resolvers = {
   Query: {
-    // We must not return a promise in a resolver
     async classes() {
       // Resolve the promise and return the result (not a promise)
       return await classController.index();
